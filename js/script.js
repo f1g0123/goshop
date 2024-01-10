4f44ce09cd7dc87da2ab85f31ddabb8c0f1e67bc
f1g0123 <figopratama28@gmail.com>
2024-01-10 09:32:55 +0700

    memperbaiki bug di navbar

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 //  Toggel class active
-const navbarnav = document.querySelector(".navbar-nav");
+const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di click
-document.querySelector("#hamburger-menu");
-onclick = () => {
-  navbarnav.classList.toggle("active");
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
 // klik di luar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
 });
