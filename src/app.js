0c16f3f710ecf7bc1f7c91ae6c34010f6c51457f
f1g0123 <figopratama28@gmail.com>
2024-01-15 10:52:50 +0700

    menambahkan fitur cart

--- src/app.js
+++ src/app.js
@@ -8,4 +8,44 @@ document.addEventListener("alpine:init", () => {
       { id: 1, name: "Sumatra Mandheling", img: "5.jpg", price: 40000 },
     ],
   }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      // cek apakah ada barang yang sama di items
+      const cartItem = this.item.find((item) => item.id === newItem.id);
+      //  jka belum ada / cart masi kosong
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        // jika barang sudah ada , cek apakah barang beda atau sama dengan yanga ada di cart
+        this.items = this.items.map((item) => {
+          // jiak barang berbeda
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            // jika barang sudah ada, tambah qunatity dan subtota nya
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+  });
 });
+
+// konversi ke rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
