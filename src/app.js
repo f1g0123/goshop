9a671915efbe9ef7d0aacf2d0cfd05df399f734c
f1g0123 <figopratama28@gmail.com>
2024-01-16 10:10:27 +0700

    memperbarui javascript

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ document.addEventListener("alpine:init", () => {
   Alpine.data("products", () => ({
     items: [
       { id: 1, name: "Robusta Brazil", img: "1.jpg", price: 20000 },
-      { id: 1, name: "Arabica Blend", img: "2.jpg", price: 25000 },
-      { id: 1, name: "Primo Passo", img: "3.jpg", price: 30000 },
-      { id: 1, name: "Aceh Gayo", img: "4.jpg", price: 35000 },
-      { id: 1, name: "Sumatra Mandheling", img: "5.jpg", price: 40000 },
+      { id: 2, name: "Arabica Blend", img: "2.jpg", price: 25000 },
+      { id: 3, name: "Primo Passo", img: "3.jpg", price: 30000 },
+      { id: 4, name: "Aceh Gayo", img: "4.jpg", price: 35000 },
+      { id: 5, name: "Sumatra Mandheling", img: "5.jpg", price: 40000 },
     ],
   }));
 
@@ -28,7 +28,7 @@ document.addEventListener("alpine:init", () => {
           if (item.id !== newItem.id) {
             return item;
           } else {
-            // jika barang sudah ada, tambah qunatity dan subtota nya
+            // jika barang sudah ada, tambah qunatity dan subtotal nya
             item.quantity++;
             item.total = item.price * item.quantity;
             this.quantity++;
